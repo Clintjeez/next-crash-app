8d5bb9bdccfc017d70e7840c3e91565802a50e18
clintjeez <clintjeez@gmail.com>
2021-05-13 00:36:41 +0100

    app update

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      <head>
+        <title>Webdev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </head>
       <h1>Welcome to next</h1>
     </div>
   );
